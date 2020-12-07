ee28539a8fd968f6c283411b0e4a4cbb792adf38
Roddick Osipov <roddick@Roddicks-MacBook-Pro.local>
2020-12-07 13:38:52 -0500

    post answers updates

---
+++ endpoint.php
@@ -0,0 +1,36 @@
+<?php
+
+// endpoint.php is where the plugin code handles the custom endpoint task of return all of the rdk_post meta values in JSON :w
+
+/* Get and return all of the posts' metavalues of the custom type Roddick post aka "rdk_post" 
+ *
+ * @param array $data Options for the function.
+ * @return array $result Post title:metavalues for all rdk_post posts or null if none exist.
+ */
+function get_all_rdk_posts_metavalue($data) {
+    $result = [];
+
+    //Here is where one would account for security; check nonce, check user role, etc.
+
+    $posts = get_posts(array( //get all posts by our custom post type 
+        'post_type' => 'rdk_post',
+    ));
+
+    if (!empty($posts)) { //if there are posts of type 'rdk_post'...
+        foreach($posts as $post) { //...push the resulting value to the end of our array
+           array_push($result, get_post_meta($post->ID, '_rdk_meta_key')[0]);
+        } 
+    }
+
+    $response = new WP_REST_RESPONSE($result);
+    return $response;
+}
+
+//init our 'get all the posts metavalue' route 
+add_action('rest_api_init', function() {
+    register_rest_route('thrive_global/', '/roddick_posts', array(
+        'methods' => 'GET',
+        'callback' => 'get_all_rdk_posts_metavalue',
+    ));
+});
+?>
---
+++ roddick-custom-post.php
@@ -0,0 +1,88 @@
+<?php
+
+/**
+ * @package Roddick Custom Post
+ * @version 0.1
+ */
+/*
+Plugin Name: Roddick Custom Post
+Description: A plugin that enables the use of a custom post type and demonstrates the use of the Wordpress REST API by making its metavalue accessible via a custom endpoint.
+Author: Roddick Osipov
+Version: 0.1
+*/
+
+include 'endpoint.php';
+
+// roddick-custom-post.php is responsible for the core plugin logic - Our custom post type and its associated values are defined and registered here.
+
+/* Register our custom post type function with the appropriate arguments
+ * @return void 
+ */
+if (!function_exists('rdk_custom_post_type')) { 
+    function rdk_custom_post_type() {
+        register_post_type('rdk_post',
+            array('labels'      => 
+                  array(
+                      'name'          => __('Custom Roddick Post', 'textdomain'),
+                      'singular_name' => __('Custom Roddick Post', 'textdomain'),
+                  ),
+                  'public'      => true,
+                  'has_archive' => true,
+                  'rewrite' => array( 'slug' => 'thrive_global'), //'thrive_global' slug - for branding
+                  'show_in_rest' => true,  //We want this available to the REST api 
+                  'supports' => array('editor', 'title'), //we want gutenberg editor, title field
+                )
+        );
+    }
+}
+
+
+/* Generate the HTML for our metavalue input field, prepopulating it if the value already exists
+ * @param array $post Post values 
+ * @return html
+ */
+if (!function_exists('rdk_metabox_html')) { 
+    function rdk_metabox_html($post) {
+        //get our existing meta value for this post (if it exists) 
+        $existing_rdk_value = get_post_meta($post->ID, '_rdk_meta_key', true); 
+        ?>
+        <label for="rdk_meta_field">Your meta value goes here!</label>
+        <input type="text" id="rdk_meta_field" name="rdk_meta_field" value="<?php empty($existing_rdk_value) ? print(""):print($existing_rdk_value) ?>" class="metabox_string_field" 
+        <?php
+    }
+}
+
+/* Register meta input field.
+ * @return void
+ */
+if (!function_exists('rdk_add_meta_boxes')) { 
+    function rdk_add_meta_boxes() {
+        add_meta_box( 'rdk',
+                      __( 'Roddick\'s Custom Metabox', 'rdk' ),
+                      'rdk_metabox_html',  //our Callback
+                      null, 
+                      'side', 
+                      'high', 
+                      'rdk_post' ); // The custom post type we just registered above
+        }
+}
+
+/* Save the our custom meta value
+ * @return void
+ */
+if (!function_exists('rdk_save_postdata')) { 
+    function rdk_save_postdata($post_id) {
+        //typically one would perform data validation here
+        if (array_key_exists('rdk_meta_field', $_POST)) {
+            update_post_meta(
+                $post_id,
+                '_rdk_meta_key',
+                $_POST['rdk_meta_field'] //value
+            );
+        }
+    }
+}
+
+add_action('add_meta_boxes', 'rdk_add_meta_boxes');
+add_action('save_post', 'rdk_save_postdata');
+add_action('init', 'rdk_custom_post_type');
